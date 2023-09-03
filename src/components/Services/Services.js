48c019ae491657594bf98469f19ce7913e46ac2c
vitrinekast <roosschuurmans@gmail.com>
2023-09-03 10:58:59 +0200

    fix client side deploy error

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -51,18 +51,18 @@ export default function Services() {
     ["Brand Guide", "Interviews", "Discovery"],
   ];
 
-  useEffect(() => {
-    if(hasScrollTrigger) {
-      gsap.registerPlugin(ScrollTrigger);
+  // useEffect(() => {
+  //   if(hasScrollTrigger) {
+  //     gsap.registerPlugin(ScrollTrigger);
 
-      ScrollSmoother.create({
-        smooth: 1,               // how long (in seconds) it takes to "catch up" to the native scroll position
-        effects: true,           // looks for data-speed and data-lag attributes on elements
-        smoothTouch: 0.1,        // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
-      });
+  //     ScrollSmoother.create({
+  //       smooth: 1,               // how long (in seconds) it takes to "catch up" to the native scroll position
+  //       effects: true,           // looks for data-speed and data-lag attributes on elements
+  //       smoothTouch: 0.1,        // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
+  //     });
 
-    }
-  }, [hasScrollTrigger])
+  //   }
+  // }, [hasScrollTrigger])
 
   useEffect(() => {
     const duration = 8;
